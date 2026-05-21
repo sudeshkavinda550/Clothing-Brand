613fd164a28640736fc8667e97321628e953782f
Sudesh Kavinda <sudeshkavinda550@gmail.com>
2026-05-21 18:40:01 +0530

    Fix deploy issue

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,13 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
   }
 })
